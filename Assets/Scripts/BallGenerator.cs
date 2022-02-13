50b2bb38a7ea03f635f1fd904d459a0d4d90b5f0
zagan-the-gun <zagan.the.gun@gmail.com>
2022-02-13 21:33:32 +0900

    おk

---
+++ Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+
+    [SerializeField] GameObject ballPrefab = default;
+    [SerializeField] Sprite[] ballSprites = default;
+
+    public IEnumerator Spawns(int count)
+    {
+        for (int i=0; i< count; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-0.2f, 0.2f), 8f);
+            GameObject ball = Instantiate(ballPrefab, pos, Quaternion.identity);
+            int ballID = Random.RandomRange(0, ballSprites.Length);
+            ball.GetComponent<SpriteRenderer>().sprite = ballSprites[ballID];
+            ball.GetComponent<Ball>().id = ballID;
+
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
+}
